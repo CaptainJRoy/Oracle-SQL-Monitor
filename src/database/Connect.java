63b3d07d1731adb96b948f8218a0c2a6c6d7b4e2
João Miguel <CaptainJRoy@github.com>
2017-11-21 10:36:22 +0000

    Ligação a funcionar

--- src/database/Connect.java
+++ src/database/Connect.java
@@ -6,23 +6,31 @@ import java.sql.SQLException;
 
 public class Connect {
 
-    private static final String USERNAME = "sys";
-    private static String PASSWORD = "oracle";
-    private static final String URL = "127.0.0.1:5502";
-    private static final String SCHEMA = "orcl";
-
-
-    static {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-
     public static Connection connect() throws SQLException {
-        return DriverManager.getConnection("jdbc:oracle:thin:127.0.0.1:5501:orcl12c", "sys", "oracle");
+        System.out.println("-------- Oracle JDBC Connection Testing ------");
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your Oracle JDBC Driver?");
+            e.printStackTrace();
+            return null;
+        }
+
+        System.out.println("Oracle JDBC Driver Registered!");
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/orcl", "system", "oracle");
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return null;
+        }
+
+        if (connection != null) System.out.println("You made it, take control your database now!");
+        else System.out.println("Failed to make connection!");
+        return connection;
     }
 
 }
--- src/dbmonitor/DBMonitor.java
+++ src/dbmonitor/DBMonitor.java
@@ -20,29 +20,8 @@ public class DBMonitor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("-------- Oracle JDBC Connection Testing ------");
-
-        try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Where is your Oracle JDBC Driver?");
-            e.printStackTrace();
-            return;
-        }
-
-        System.out.println("Oracle JDBC Driver Registered!");
-        Connection connection = null;
-
-        try {
-            connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/orcl", "system", "oracle");
-        } catch (SQLException e) {
-            System.out.println("Connection Failed! Check output console");
-            e.printStackTrace();
-            return;
-        }
-
-        if (connection != null) System.out.println("You made it, take control your database now!");
-        else System.out.println("Failed to make connection!");
+        Connection connection = Connect.connect();
+        connection.close();
     }
     
 }
