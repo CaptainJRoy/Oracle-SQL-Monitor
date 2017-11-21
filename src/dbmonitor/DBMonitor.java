11fa020104a8fc9afb8aebc9f0aa043151cd28f2
João Miguel <CaptainJRoy@github.com>
2017-11-21 10:32:10 +0000

    Ligação a funcionar

--- src/dbmonitor/DBMonitor.java
+++ src/dbmonitor/DBMonitor.java
@@ -7,9 +7,8 @@ package dbmonitor;
 
 import database.Connect;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -21,11 +20,29 @@ public class DBMonitor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("-------- Oracle JDBC Connection Testing ------");
+
         try {
-            Connection con = Connect.connect();
-        } catch (Exception e) {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your Oracle JDBC Driver?");
             e.printStackTrace();
+            return;
         }
+
+        System.out.println("Oracle JDBC Driver Registered!");
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/orcl", "system", "oracle");
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return;
+        }
+
+        if (connection != null) System.out.println("You made it, take control your database now!");
+        else System.out.println("Failed to make connection!");
     }
     
 }
