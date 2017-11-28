59ed5a1830a3dcc4d00f0d1eb6166c8ee2fd1b46
João Miguel <CaptainJRoy@github.com>
2017-11-28 10:17:35 +0000

    Obtem resultados de espaço total e espaço livre

--- src/database/Tablespaces.java
+++ src/database/Tablespaces.java
@@ -24,20 +24,45 @@ public class Tablespaces implements Runnable {
     @Override
     public void run() {
         try {
-            String getTS = "select b.tablespace_name, tbs_size SizeMb, a.free_space FreeMb\n" +
-                            "from  (select tablespace_name, round(sum(bytes)/1024/1024 ,2) as free_space\n" +
-                            "       from dba_free_space\n" +
-                            "       group by tablespace_name) a,\n" +
-                            "      (select tablespace_name, sum(bytes)/1024/1024 as tbs_size\n" +
-                            "       from dba_data_files\n" +
-                            "       group by tablespace_name) b\n" +
-                            "where a.tablespace_name(+)=b.tablespace_name";
+            String getTS =  "column \"Tablespace\" format a13\n" +
+                            "column \"Used MB\"    format 99,999,999\n" +
+                            "column \"Free MB\"    format 99,999,999\n" +
+                            "column \"Total MB\"   format 99,999,999\n" +
+                            "select\n" +
+                            "   fs.tablespace_name                          \"Tablespace\",\n" +
+                            "   (df.totalspace - fs.freespace)              \"Used MB\",\n" +
+                            "   fs.freespace                                \"Free MB\",\n" +
+                            "   df.totalspace                               \"Total MB\",\n" +
+                            "   round(100 * (fs.freespace / df.totalspace)) \"Pct. Free\"\n" +
+                            "from\n" +
+                            "   (select\n" +
+                            "      tablespace_name,\n" +
+                            "      round(sum(bytes) / 1048576) TotalSpace\n" +
+                            "   from\n" +
+                            "      dba_data_files\n" +
+                            "   group by\n" +
+                            "      tablespace_name\n" +
+                            "   ) df,\n" +
+                            "   (select\n" +
+                            "      tablespace_name,\n" +
+                            "      round(sum(bytes) / 1048576) FreeSpace\n" +
+                            "   from\n" +
+                            "      dba_free_space\n" +
+                            "   group by\n" +
+                            "      tablespace_name\n" +
+                            "   ) fs\n" +
+                            "where\n" +
+                            "   df.tablespace_name = fs.tablespace_name";
             PreparedStatement ps = this.c.prepareStatement(getTS);
             ResultSet rs = ps.executeQuery();
             System.out.println(rs);
             
             while(rs.next()) {
-                System.out.println(rs.getString(1) + "\t\t" + rs.getFloat(2));
+                System.out.println("Tablespace: " + rs.getString(1) + "\t\t" +
+                                   "Used MB: " + rs.getFloat(2) + "\t\t" +
+                                   "Free MB: " + rs.getFloat(3) + "\t\t" +
+                                   "Total MB: " + rs.getFloat(4) + "\t\t" +
+                                   "Pct. Free: " + rs.getFloat(5));
             }
         }
         catch (Exception e) {
